6d202f8e1af219b7be11b6a250098678457701a4
antgordon <56523753+antgordon@users.noreply.github.com>
2020-09-07 14:15:06 -0400

    Add the code

---
+++ SqlCleanserWPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlCleanserWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextData textData;
+        private bool loaded;
+
+
+        public MainWindow()
+        {
+            textData = new TextData();
+            InitializeComponent();
+            cleanTab.DataContext = textData;
+            rawTab.DataContext = textData;
+            loaded = true;
+ 
+
+        }
+
+        private void cleanTab_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+           
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+          
+        }
+
+  
+
+        private void cleanTab_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        
+        }
+
+        private void tabControl_Selected_1(object sender, RoutedEventArgs e)
+        {
+     
+        }
+
+        private void tabControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+      
+         
+        }
+
+        private void tabControl_Selected(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+          
+
+        }
+
+        private void tabControl_LostFocus(object sender, RoutedEventArgs e)
+        {
+            textData.RunClean();
+        }
+    }
+}
---
+++ SqlCleanserWPF/TextData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace SqlCleanserWPF
+{
+    public class TextData : INotifyPropertyChanged
+    {
+
+        private Regex symbols = new Regex(@"""|\+");
+        private Regex prepParams = new Regex(@"\?");
+        private string _rawtext;
+        private string _cleantext;
+
+
+        public TextData() {
+            _rawtext = "";
+            _cleantext = "";
+        }
+
+        public string RawText { 
+            set {
+                _rawtext = value;
+
+                OnPropertyChanged("RawText");
+            }
+            get => _rawtext; 
+        }
+
+
+        public string CleanText
+        {
+            set
+            {
+                _cleantext = value;
+
+                OnPropertyChanged("CleanText");
+            }
+            get => _cleantext;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void RunClean()
+        {
+
+            String intermediate = RawText;
+            intermediate = cleanSymbols(intermediate);
+            intermediate = cleanPrepParams(intermediate);
+
+
+            CleanText = intermediate;
+        }
+
+        private String cleanSymbols(String input) 
+        {
+            return symbols.Replace(input, "");
+        }
+
+        private String cleanPrepParams(String input)
+        {
+            return prepParams.Replace(input, "1");
+        }
+
+    }
+}
